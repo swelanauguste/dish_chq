0dd7107cb9dc2ff694b5ca0e5685d2e09a66cb98
Swelan Auguste <swelanauguste@gmail.com>
2023-09-22 19:27:45 -0400

    settings

--- app/cf/settings.py
+++ app/cf/settings.py
@@ -9,13 +9,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-410rdl2nv&n!cgv%wgg)+zvlzeo%_u2mer3eq-!7*&dwoo#s(w"
-# SECRET_KEY = os.environ.get("SECRET_KEY")
+# SECRET_KEY = "django-insecure-410rdl2nv&n!cgv%wgg)+zvlzeo%_u2mer3eq-!7*&dwoo#s(w"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = ["localhost", "dis-chq.kingship.info"]
+# ALLOWED_HOSTS = ["localhost", "dis-chq.kingship.info"]
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+
 
 CSRF_TRUSTED_ORIGINS = ["https://dis-chq.kingship.info"]
 
